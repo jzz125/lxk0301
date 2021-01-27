24ca8c1ae4c18b6e0b8024402798bcb4e1995eb5
aswqazx <454850527@qq.com>
2021-01-27 10:59:36 +0800

    update

--- jd_cash.js
+++ jd_cash.js
@@ -29,8 +29,9 @@ let cookiesArr = [], cookie = '', message;
 let helpAuthor = true;
 const randomCount = $.isNode() ? 20 : 5;
 const inviteCodes = [
-  `-4msulYas0O2JsRhE-2TA5XZmBQ@eU9Yar_mb_9z92_WmXNG0w@eU9YaO7jMvwh-W_VzyUX0Q`,
-  `-4msulYas0O2JsRhE-2TA5XZmBQ@eU9Yar_mb_9z92_WmXNG0w@eU9YaO7jMvwh-W_VzyUX0Q`
+  `eU9Yau7nMP8moDiAz3NFhQ@eU9YPrHtAZtnlhWHnQJi`,
+  `ZUJ1Jb3jJKV4oA@eU9YPrHtAZtnlhWHnQJi`,
+  `ZUJ1Jb3jJKV4oA@eU9Yau7nMP8moDiAz3NFhQ`
 ]
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
